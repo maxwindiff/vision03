e3684c862823b6f03e0fbda9b730d3e1be4f4e28
Max Ng <maxwindiff@gmail.com>
2024-05-25 17:34:06 -0700

    animate

--- vision03/ImmersiveView.swift
+++ vision03/ImmersiveView.swift
@@ -27,44 +27,56 @@ struct ImmersiveView: View {
   let worldTracking = WorldTrackingProvider()
   let headAnchor = AnchorEntity(.head)
 
+  let startDate = Date.now
+
   @State var spherePosition: SIMD3<Float> = [0, 1.5, -1]
   @State var sphereLookAt: SIMD3<Float> = [0, 0, -1]
-  @State var sphereEntity: Entity?
+  @State var sphereEntity: ModelEntity?
 
   var body: some View {
-    RealityView { content, attachments in
-      content.add(headAnchor)
-      
-      do {
-        let mesh = MeshResource.generateSphere(radius: 2)
-        try await mesh.invert()
-        let mat = try await ShaderGraphMaterial(named: "/Root/MyMaterial", from: "Immersive.usda", in: realityKitContentBundle)
-        let entity = ModelEntity(mesh: mesh, materials: [mat])
-        entity.look(at: sphereLookAt, from: spherePosition, relativeTo: nil, forward: .positiveZ)
-        content.add(entity)
-        sphereEntity = entity
-      } catch {
-        print("Failed to create sphere: \(error)")
-        return;
-      }
-
-//      if let panel = attachments.entity(for: "sphere-controller") {
-//        panel.look(at: [0, 0, 0.5], from: [0, -0.25, -0.8],
-//                   relativeTo: nil, forward: .positiveZ)
-//        headAnchor.addChild(panel)
-//      }
-    } attachments: {
-      Attachment(id: "sphere-controller") {
-        PositionControl(title: "Sphere",
-                        position: $spherePosition,
-                        lookAt: $sphereLookAt)
-        .onChange(of: [spherePosition, sphereLookAt]) {
-          sphereEntity!.look(at: sphereLookAt, from: spherePosition, relativeTo: nil, forward: .positiveZ)
+    TimelineView(.animation) { context in
+      RealityView { content, attachments in
+        do {
+          let mesh = MeshResource.generateSphere(radius: 2)
+          try await mesh.invert()
+          let mat = try await ShaderGraphMaterial(named: "/Root/MyMaterial", from: "Immersive.usda", in: realityKitContentBundle)
+          let entity = ModelEntity(mesh: mesh, materials: [mat])
+          entity.name = "Sphere"
+          entity.look(at: sphereLookAt, from: spherePosition, relativeTo: nil, forward: .positiveZ)
+          content.add(entity)
+          sphereEntity = entity
+        } catch {
+          print("Failed to create sphere: \(error)")
+          return;
+        }
+        
+        //      content.add(headAnchor)
+        //      if let panel = attachments.entity(for: "sphere-controller") {
+        //        panel.look(at: [0, 0, 0.5], from: [0, -0.25, -0.8],
+        //                   relativeTo: nil, forward: .positiveZ)
+        //        headAnchor.addChild(panel)
+        //      }
+      } update: { content, attachments in
+        // TODO: can't use @State from here?
+        let elapsed = context.date.timeIntervalSince(startDate)
+        if let entity = content.entities.first(where: { $0.name == "Sphere" }) as? ModelEntity,
+           var mat = entity.model?.materials.first as? ShaderGraphMaterial {
+          try? mat.setParameter(name: "Time", value: .float(Float(elapsed)))
+          entity.model?.materials = [mat]
+        }
+      } attachments: {
+        Attachment(id: "sphere-controller") {
+          PositionControl(title: "Sphere",
+                          position: $spherePosition,
+                          lookAt: $sphereLookAt)
+          .onChange(of: [spherePosition, sphereLookAt]) {
+            sphereEntity!.look(at: sphereLookAt, from: spherePosition, relativeTo: nil, forward: .positiveZ)
+          }
         }
       }
-    }
-    .task {
-      try? await session.run([worldTracking])
+      .task {
+        try? await session.run([worldTracking])
+      }
     }
   }
 }
--- vision03/vision03App.swift
+++ vision03/vision03App.swift
@@ -3,11 +3,7 @@ import SwiftUI
 @main
 struct vision03App: App {
   var body: some Scene {
-    WindowGroup(id: "main") {
-      ContentView()
-    }.windowStyle(.volumetric)
-    
-    ImmersiveSpace(id: "space") {
+    ImmersiveSpace {
       ImmersiveView()
     }
   }
